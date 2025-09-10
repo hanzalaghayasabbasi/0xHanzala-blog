2c1247f2f2b542572072c6a67011ab191dadbe23
Hanzala Ghayas Abbasi <123712590+hanzalaghayasabbasi@users.noreply.github.com>
2025-09-10 19:24:07 +0500

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,10 +3,10 @@ export const PAGE_SIZE = 8;
 export const LIGHT_MODE = "light",
 	DARK_MODE = "dark",
 	AUTO_MODE = "auto";
-export const DEFAULT_THEME = AUTO_MODE;
+export const DEFAULT_THEME = DARK_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
+export const BANNER_HEIGHT = 35 ;
 export const BANNER_HEIGHT_EXTEND = 30;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
